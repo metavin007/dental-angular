f4d81a3996dcf9a49b3f77d27a30afa917d92152
metavin007 <meta.vin@hotmail.com>
2015-06-20 10:46:01 +0700

    firstcommit

---
+++ src/main/java/th/co/geniustree/dental/angular/controller/CustomerController.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package th.co.geniustree.dental.angular.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import th.co.geniustree.dental.angular.model.Customer;
+import th.co.geniustree.dental.angular.model.User;
+import th.co.geniustree.dental.angular.repo.CustomerRepo;
+import th.co.geniustree.dental.angular.repo.UserRepo;
+
+
+
+
+/**
+ *
+ * @author pramoth
+ */
+@RestController
+public class CustomerController {
+
+    @Autowired
+    private CustomerRepo customerRepo;
+    private UserRepo userRepo;
+
+    @RequestMapping(value = "/customers")
+    public Page<Customer> getCustomers(Pageable pageable) {
+        return customerRepo.findAll(pageable);
+    }
+    @RequestMapping(value = "/customers",method = RequestMethod.POST)
+    public void saveCustomer(@Validated @RequestBody Customer customer) {
+        customerRepo.save(customer);
+    }
+     ////////// Delete ////////////////////
+    @RequestMapping(value = "/customerde", method = RequestMethod.POST)
+    public void deleteCustomer(@RequestBody Customer customer) {
+        customerRepo.delete(customer.getId());
+    }
+    @RequestMapping(value = "/login",method = RequestMethod.POST)
+    public boolean loginAdmin(@RequestBody User user){
+       return userRepo.equals(user);
+    }
+}
---
+++ src/main/java/th/co/geniustree/dental/angular/service/MyUserDetailService.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package th.co.geniustree.dental.angular.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import th.co.geniustree.dental.angular.repo.UserRepo;
+
+
+/**
+ *
+ * @author User
+ */
+@Service
+public class MyUserDetailService implements UserDetailsService{
+    @Autowired
+    private UserRepo userRepo;
+    
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+       UserDetails user = userRepo.findOne(username);
+       return user;
+    }
+}
