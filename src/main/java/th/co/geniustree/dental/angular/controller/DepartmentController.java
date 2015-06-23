8feb31ca6f5d7ec46ae2fad7c23143ce201bdd6f
metavin007 <meta.vin@hotmail.com>
2015-06-23 14:50:57 +0700

    save21

--- src/main/java/th/co/geniustree/dental/angular/controller/DepartmentController.java
+++ src/main/java/th/co/geniustree/dental/angular/controller/DepartmentController.java
@@ -45,15 +45,7 @@ public class DepartmentController {
 
     @RequestMapping(value = "/department/search", method = RequestMethod.POST)
     public Page<Department> searchDepartment(@RequestBody String keyword, Pageable pageable) {
-        
-        if(keyword.equals("s"))
-        {
-           return departmentRepo.findAll(pageable);
-        }
-        else{
            return departmentRepo.findByName(keyword, pageable);
-        }
-        
     }
     
 }
