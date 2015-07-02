6a93835b2d78c8f0ca807653d56c069d8a5b75c9
metavin007 <meta.vin@hotmail.com>
2015-07-02 10:45:05 +0700

    save77

--- src/main/webapp/js/app/doctor.js
+++ src/main/webapp/js/app/doctor.js
@@ -8,13 +8,15 @@ angular.module('doctor').controller('doctorController',function ($scope,$http){
             function loaddoctor() {
                 $http.get('/showdoctors').success(function (data) {
                     $scope.doctors = data; 
-                }).error(function (data, status, header, config) {
+                }).error(function (data, status, header, config) {                    
                 });
             }
             
              $scope.savedoctor = function () {
-                $http.post('/savedoctors', $scope.doctor).success(function (data) {                  
+                $http.post('/savedoctors', $scope.doctor).success(function (data) {  
+                    alert('saveข้อมูลเรียบร้อย');
                 }).error(function (data, status, header, config) {
+                    alert('ท่านกรอกข้อมูลไม่สมบูรณ์');
                     $scope.error = data;                   
                 });
             };
